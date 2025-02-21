17e0058c970cb58225b96f1722e5a9a8f4e423d5
redbyte09 <sakshisharma4234@gmail.com>
2025-02-21 14:05:51 +0530

    [Sakshi] solved GET Request Method and pass name as                 path variable

---
+++ show_hello_usingpathvariable/src/main/java/com/bridgelab/show_hello_usingpathvariable/ShowHelloUsingpathvariableApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelab.show_hello_usingpathvariable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowHelloUsingpathvariableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowHelloUsingpathvariableApplication.class, args);
+	}
+
+}
---
+++ show_hello_usingpathvariable/src/test/java/com/bridgelab/show_hello_usingpathvariable/ShowHelloUsingpathvariableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelab.show_hello_usingpathvariable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowHelloUsingpathvariableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
