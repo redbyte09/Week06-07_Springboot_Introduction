9271f86deddaf54fa7fb365b80e1144a5e105980
redbyte09 <sakshisharma4234@gmail.com>
2025-02-21 12:33:37 +0530

    git commit -m "[Sakshi] Show Hello from BridgeLab "

---
+++ show_hello/src/main/java/com/bridgelab/show_hello/ShowHelloApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelab.show_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowHelloApplication.class, args);
+	}
+
+}
---
+++ show_hello/src/test/java/com/bridgelab/show_hello/ShowHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelab.show_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
